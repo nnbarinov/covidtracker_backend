8dba8c6b73e59d25366cf67263e8708a77f7bff4
Nikolai Barinov <nbarinov@gmail.com>
2022-02-26 21:51:34 +0300

    changed database to MariaDB

--- covidtracker_backend/settings.py
+++ covidtracker_backend/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'covidtracker_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'covid',
+        'USER': 'covid',
+        'PASSWORD': '40Njd;c,',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
